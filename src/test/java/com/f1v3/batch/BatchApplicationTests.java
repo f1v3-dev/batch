aaa631398778ea170f5174406c956ac7c52e81a6
seungjo <f1v3@kakao.com>
2025-08-28 00:02:14 +0900

    chore: first commit

---
+++ src/test/java/com/f1v3/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1v3.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
