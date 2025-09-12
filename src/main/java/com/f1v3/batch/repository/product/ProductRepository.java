683f946042e7ea6b38c2ccd83b71e0f2c844b862
seungjo <f1v3@kakao.com>
2025-09-12 22:06:47 +0900

    feat: Product Delete 메서드 추가

--- src/main/java/com/f1v3/batch/repository/product/ProductRepository.java
+++ src/main/java/com/f1v3/batch/repository/product/ProductRepository.java
@@ -2,7 +2,12 @@ package com.f1v3.batch.repository.product;
 
 import com.f1v3.batch.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @Modifying
+    @Query("DELETE FROM Product")
+    void deleteAllProducts();
 }
